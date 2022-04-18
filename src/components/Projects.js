5c7bae5c1a7539d513278a9038a4ccbebb0dc647
ji <jiyongprk@gmail.com>
2022-04-18 16:19:35 -0400

    make next btn function

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,12 +3,46 @@ import projectDatas from '../data'
 
 export default function Projects() {
 
-    const projectList = document.querySelectorAll('.projects')
-    console.log(projectList)
-    // projectList[0].scrollIntoView()
+    const [projectNumber, setProjectNumber] = useState(0)
+
+    const nextBtn = () =>{
+        const projects = document.querySelectorAll('.projects');
+        if (projectNumber === 0 ) {
+            projects[1].scrollIntoView()
+            setProjectNumber(1);
+        } else if (projectNumber === 1){
+            projects[2].scrollIntoView()
+            setProjectNumber(2);
+        } else if (projectNumber === 2){
+            projects[3].scrollIntoView()
+            setProjectNumber(3);
+        } else if (projectNumber === 3){
+            projects[4].scrollIntoView()
+            setProjectNumber(4);
+        } else if (projectNumber === 4){
+            projects[5].scrollIntoView()
+            setProjectNumber(5);
+        } else if (projectNumber === 5){
+            projects[6].scrollIntoView()
+            setProjectNumber(6);
+        } else if (projectNumber === 6){
+            projects[7].scrollIntoView()
+            setProjectNumber(7);
+        } else if (projectNumber === 7){
+            projects[8].scrollIntoView()
+            setProjectNumber(8);
+        } else if (projectNumber === 8){
+            projects[9].scrollIntoView()
+            setProjectNumber(9);
+        }
+
+    }
+    
+    
+
 
     const showAllProjects = projectDatas.map((projectData)=>{
-        if(projectData.type === "javascript" || projectData.type === "react"){
+        if(projectData.type === "Javascript" || projectData.type === "React"){
             return (
                 <div className='projects' key={projectData.id}>
                     <div className='project-picture-container'>
@@ -55,9 +89,16 @@ export default function Projects() {
                 <button>React</button>
                 {/* <button>Node</button> */}
             </div>
-            
+            <h2>Javascript</h2>
             <div className='projects-container'>
-                {showProjects}
+                <button className='prev-btn'>&#8678;</button>
+                <div className='projects-list'>
+                    {showProjects}
+                </div>
+                <button className='next-btn' onClick={nextBtn}>&#8680;</button>
+            </div>
+            <div>
+                <h3>Page Number: {projectNumber} of {projectDatas.length}</h3>
             </div>
 
         </div>
