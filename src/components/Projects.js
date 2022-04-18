d66f851b0625e850dee8fd3d83ecfd695224abe3
ji <jiyongprk@gmail.com>
2022-04-18 14:48:32 -0400

    add projects to display, style the project container

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,8 +6,32 @@ export default function Projects() {
     const showAllProjects = projectDatas.map((projectData)=>{
         if(projectData.type === "javascript" || projectData.type === "react"){
             return (
-                <div className='projects'>
-                    <p>{projectData.name}</p>
+                <div className='projects' key={projectData.id}>
+                    <div className='project-picture-container'>
+                        <img src={projectData.img} className="project-picture"/>
+                    </div>
+                    <div className='project-description-container'>
+                        <div>
+                            <p>Project Name:</p>
+                            <h1>{projectData.name}</h1>
+                        </div>
+                        <div>
+                            <p>Description: </p>
+                            <h2>{projectData.description}</h2>
+                        </div>
+                        <div>
+                            <p>Language: </p>
+                            <h3>{projectData.type}</h3>
+                        </div>
+                        <div>
+                            <p>Demo: </p>
+                            <h3>{projectData.live}</h3>
+                        </div>
+                        <div>
+                            <p>Source: </p>
+                            <h3>{projectData.repo}</h3>
+                        </div>
+                    </div>
                 </div>
             )
         }
