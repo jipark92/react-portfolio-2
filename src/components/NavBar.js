49dc08fc292b3f142ec2a034daef7f6cc2543238
ji <jiyongprk@gmail.com>
2022-04-18 19:45:44 -0400

    change font size smaller in mobil view

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,18 +3,24 @@ import {Link} from 'react-router-dom'
 
 export default function NavBar() {
 
+    const blah = (e) => {
+        if (e.target.parentNode) {
+            e.target.parentNode.style.color = "blue"
+        } 
+    }
+
     return (
         <header>
             <nav>
                 <ul>
                     <Link to='/'>
-                        <li><h1>About Me</h1></li>
+                        <li onClick={blah}><h1>About Me</h1></li>
                     </Link>
                     <Link to='/projects'>
-                        <li><h1>Projects</h1></li>
+                        <li onClick={blah}><h1>Projects</h1></li>
                     </Link>
                     <Link to='/contact'>
-                        <li><h1>Contact</h1></li>
+                        <li onClick={blah}><h1>Contact</h1></li>
                     </Link>
                 </ul>
             </nav>
