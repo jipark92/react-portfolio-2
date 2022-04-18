ae1644c6fbf5bdc6549f60eeee994026c71b50e5
ji <jiyongprk@gmail.com>
2022-04-18 14:20:05 -0400

    flex direction to column mobile view

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,17 +4,16 @@ export default function AboutMe() {
     return (
         <div className='information-container'>
             <div className='about-me-container'>
-                <div className='profile-image'>
+                <div className='profile-picture-container'>
                     <img src="https://avatars.githubusercontent.com/u/93008833?v=4" alt="profile-pictures" className='profile-picture'/>
                 </div>
                 <h1>Hi, I am Ji Young Park</h1>
-                <h2>Frontend Engineer</h2>
+                <h2><u>Frontend Engineer</u></h2>
             </div>
             <div className='about-me-container-2'>
                 <h2>Can an break-dancer teach themself to program? This portfolio website can be your answer.</h2>
                 <h2>Web development allows me to express my creativity in a more satisfying way, to create with clear structure and to focus in on details in a way I've never been able to do before.</h2>
                 <h2>place holder A self learned frontend engineer </h2>
-                <h2>place holder</h2>
             </div>
         </div>
     )
