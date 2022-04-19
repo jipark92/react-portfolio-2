68ab4bb353d5291aa0dd139059f4871dddd03bc1
ji <jiyongprk@gmail.com>
2022-04-19 00:06:00 -0400

    clean up

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -14,17 +14,23 @@ export default function AboutMe() {
             <div className='about-me-container-2'>
                 <h2>Can an self-taught breakdancer teach themself to program as well? This portfolio website can be your answer.</h2>
                 <h2>Web development allows me to express my creativity in a more satisfying way, to create with clear structure and to focus in on details in a way I've never been able to do before.</h2>
-                <h2>As someone with a keen interest in tech and a knack for problem-solving, web development was an obvious path.</h2>
+                <h2>As someone with a keen interest in technology and a knack for problem-solving, web development was an obvious path.</h2>
                 <div className='favorite-tools'>
                     <h3>My Skills: </h3>
-                    <img src="https://img.icons8.com/office/344/react.png" />
+                    <img src="https://camo.githubusercontent.com/27d0b117da00485c56d69aef0fa310a3f8a07abecc8aa15fa38c8b78526c60ac/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f72656163742f72656163742d6f726967696e616c2e737667" />
                     <img src="https://img.icons8.com/color/344/javascript--v1.png" />
-                    <img src="https://img.icons8.com/ios-filled/344/html-5--v1.png" />
-                    <img src="https://img.icons8.com/ios-filled/344/css.png" />
-                    <img src="https://img.icons8.com/ios-filled/344/github.png" />
+                    <img src="https://camo.githubusercontent.com/d458b55282fc167f5a189b35e54f966acdd5100d9331d90bea6416f2805e7f95/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f68746d6c352f68746d6c352d706c61696e2e737667" />
+                    <img src="https://camo.githubusercontent.com/ad8fbf7f75f04b296b72beb893acf572b364e69ec35ea41a68a29507f5b1cd1b/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f637373332f637373332d706c61696e2e737667" />
+                    <img src="https://camo.githubusercontent.com/964a169bbc7417bcf2b1ee0ddd2122d9592a50dee693f9421428bdd11d32c18e/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f626f6f7473747261702f626f6f7473747261702d706c61696e2e737667" />
+                    <img src="https://camo.githubusercontent.com/dc9e7e657b4cd5ba7d819d1a9ce61434bd0ddbb94287d7476b186bd783b62279/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6769742f6769742d6f726967696e616c2e737667" />
+                </div>
+                <div className='learning-tools'>
+                    <h3>Learning Skills: </h3>
+                    <img src="https://camo.githubusercontent.com/900baefb89e187c8b32cdbb3b440d1502fe8f30a1a335cc5dc5868af0142f8b1/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6e6f64656a732f6e6f64656a732d6f726967696e616c2e737667" />
+                    <img src="https://camo.githubusercontent.com/40756575fc2fd74b1883ea0cc5c2a49aa7048ab58286f43a121109d69a9ea160/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f657870726573732f657870726573732d6f726967696e616c2e737667" />
+                    <img src="https://camo.githubusercontent.com/9ebde7ca22ab3f3b4bf92d2743804ab9e581e413a16cdf3626c2092e69967d80/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6d6f6e676f64622f6d6f6e676f64622d6f726967696e616c2e737667" />
                 </div>
             </div>
-            <div></div>
         </div>
     )
 }
