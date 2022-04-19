2f90ab51a6bab204585ba043ba6f7348d34b2bf6
ji <jiyongprk@gmail.com>
2022-04-19 00:39:41 -0400

    change id value 0-2

--- src/reactData.js
+++ src/reactData.js
@@ -1,6 +1,6 @@
 export default [
 {
-    "id": 3,
+    "id": 0,
     "img": "https://imgur.com/0Uxfoas.png",
     "name": "CV Application",
     "description":"For this React Project, I was able to send value of what is written in the input box to show up on another page by sending props over to different components",
@@ -9,16 +9,16 @@ export default [
     "repo":"https://github.com/norfkorean/react-cvapplication-project"
 },
 {
-    "id": 37,
+    "id": 1,
     "img": "https://imgur.com/IPHj16U.png",
     "name": "Memory Card Game",
-    "description":"This was my 2nd Reactjs Project! Fun but very challenging game logic. I had to use react hooks like useState/useEffect for this project. I was able to make the cards shuffle on every click.",
+    "description":"hallenging game logic. I had to use react hooks like useState/useEffect for this project. I was able to make the cards shuffle on every click.",
     "type":"React",
     "live": "https://jipark-react-memorycard.netlify.app/",
     "repo":"https://github.com/norfkorean/react-memorycard-project"
 },
 {
-    "id": 39,
+    "id": 2,
     "img": "https://imgur.com/hBbnxxR.png",
     "name": "Webstore",
     "description":"Webstore built with react BrowserRouter, Routes,Route",
