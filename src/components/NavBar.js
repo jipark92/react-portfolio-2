6e72c7ecb2c6e7fdc8fa31e4a396c03868ae1e44
ji <jiyongprk@gmail.com>
2022-04-19 00:06:24 -0400

    add colors to nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,13 @@ import {Link} from 'react-router-dom'
 export default function NavBar() {
 
     const blah = (e) => {
-        if (e.target.parentNode) {
-            e.target.parentNode.style.color = "blue"
-        } 
+        if (e.target.parentNode.id === "1") {
+            e.target.parentNode.style.color = "rgb(0, 204, 255)"
+        } else if (e.target.parentNode.id === "2"){
+            e.target.parentNode.style.color = "rgb(33,201,152)"
+        } else if (e.target.parentNode.id === "3"){
+            e.target.parentNode.style.color = "rgb(241, 94, 119)"
+        }
     }
 
     return (
@@ -14,13 +18,13 @@ export default function NavBar() {
             <nav>
                 <ul>
                     <Link to='/'>
-                        <li onClick={blah}><h1>About Me</h1></li>
+                        <li onClick={blah} id="1"><h1>About Me</h1></li>
                     </Link>
                     <Link to='/projects'>
-                        <li onClick={blah}><h1>Projects</h1></li>
+                        <li onClick={blah} id="2"><h1>Projects</h1></li>
                     </Link>
                     <Link to='/contact'>
-                        <li onClick={blah}><h1>Contact</h1></li>
+                        <li onClick={blah} id="3"><h1>Contact</h1></li>
                     </Link>
                 </ul>
             </nav>
